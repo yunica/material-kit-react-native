bda8aceea7f18d601f8af6cf0ec9b825ac5b9ce7
Junior flores <junior@developmentseed.org>
2020-06-10 22:37:26 -0500

    add redux in app redux-logger

---
+++ app/api/index.js
@@ -0,0 +1,18 @@
+const URL = 'https://api.tvmaze.com/schedule?country=US&date=2017-03-13'
+const URL_PEOPLE = 'http://api.tvmaze.com/search/people?q=lauren'
+
+
+export const fetchSchedule = () => {
+    return fetch(URL)
+        .then(Response => {
+            return Promise.all([Response, Response.json()])
+        })
+}
+
+
+export const fetchPeople = () => {
+    return fetch(URL_PEOPLE)
+        .then(Response => {
+            return Promise.all([Response, Response.json()])
+        })
+}
